a4074cb8d683084bd3b917afbc001757b39a60bb
tarasevich-nikolai <dormidontn1@gmail.com>
2017-06-08 21:15:10 +0300

    Iterator

---
+++ src/main/java/com/tarasevich/nikolai/behaviour/iterator/ChannelIterator.java
@@ -0,0 +1,8 @@
+package com.tarasevich.nikolai.behaviour.iterator;
+
+public interface ChannelIterator {
+
+	public boolean hasNext();
+
+	public Channel next();
+}
---
+++ src/main/java/com/tarasevich/nikolai/behaviour/iterator/ChannelTypeEnum.java
@@ -0,0 +1,6 @@
+package com.tarasevich.nikolai.behaviour.iterator;
+
+public enum ChannelTypeEnum {
+
+	ENGLISH, HINDI, FRENCH, ALL;
+}
